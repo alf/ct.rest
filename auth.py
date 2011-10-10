f9e1e3739a4f453f1e84262a03fe7904f8e8657e
Alf Lervag <alf.lervag@bouvet.no>
2011-10-10 17:12:37 +0200

    Exposed some data through our API

--- auth.py
+++ auth.py
@@ -1,12 +1,43 @@
+from __future__ import absolute_import
+
 from functools import wraps
+from hashlib import sha256
 from flask import request, Response
+from flask import session, g
+
+from ct.core.apis import BaseAPI
+
+
+def get_ct_object(username, password):
+    key = get_session_key(username, password)
+    if key in session and session[key].valid_session():
+        return session[key]
+
+    if do_ct_login(username, password):
+        return get_ct_object(username, password)
+
+    return None
+
+
+def do_ct_login(username, password):
+    ct = BaseAPI("https://currenttime.bouvet.no")
+    logged_in = ct.login(username, password)
+    if logged_in:
+        key = get_session_key(username, password)
+        session[key] = ct
+
+    return logged_in
+
+
+def get_session_key(username, password):
+    return sha256("%s:%s" % (username, password)).hexdigest()
 
 
 def check_auth(username, password):
     """This function is called to check if a username /
     password combination is valid.
     """
-    return username == 'admin' and password == 'secret'
+    return get_ct_object(username, password) is not None
 
 
 def get_auth_headers():
@@ -39,5 +70,8 @@ def requires_auth(f):
         auth = request.authorization
         if not auth or not check_auth(auth.username, auth.password):
             return authenticate()
+
+        g.ct = get_ct_object(auth.username, auth.password)
+
         return f(*args, **kwargs)
     return decorated
